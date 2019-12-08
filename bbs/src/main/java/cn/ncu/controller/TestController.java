c93366a7eb6054295594eaf5608efa61fd4a72b2
zengfeng <2352661414@qq.com>
2019-12-08 11:21:18 +0800

    在TestController上加上了@Controller注解

--- bbs/src/main/java/cn/ncu/controller/TestController.java
+++ bbs/src/main/java/cn/ncu/controller/TestController.java
@@ -1,7 +1,9 @@
 package cn.ncu.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/test")
 public class TestController {
 

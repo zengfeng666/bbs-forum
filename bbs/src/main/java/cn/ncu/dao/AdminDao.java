0e85a3d2e732be865a65033cd1bbfe346a22efd2
aojun <963334657@qq.com>
2019-12-15 13:20:00 +0800

    新增管理员发布公告的功能

--- bbs/src/main/java/cn/ncu/dao/AdminDao.java
+++ bbs/src/main/java/cn/ncu/dao/AdminDao.java
@@ -10,19 +10,39 @@ import java.util.List;
 @Repository
 public interface AdminDao {
 
+    /**
+     * 置顶
+     * @param pid
+     */
     @Update("update post set is_top = 1 where pid = #{pid}")
     void isTopPost(@Param("pid")Integer pid);
 
+    /**
+     * 取消置顶
+     * @param pid
+     */
     @Update("update post set is_top = 0 where pid = #{pid}")
     void notTopPost(@Param("pid")Integer pid);
 
+    /**
+     * 加精
+     * @param pid
+     */
     @Update("update post set is_good = 1 where pid = #{pid}")
     void isGoodPost(@Param("pid")Integer pid);
 
+    /**
+     * 取消加精
+     * @param pid
+     */
     @Update("update post set is_good = 0 where pid = #{pid}")
     void notGoodPost(@Param("pid")Integer pid);
 
 
+    /**
+     * 查找所有公告
+     * @return
+     */
     @Select("select * from notice order by notice_time desc")
     @Results(id="noticeMap",
             value = {
@@ -33,5 +53,15 @@ public interface AdminDao {
                 @Result(column = "content", property = "content"),
             })
     public List<Notice> findNotice();
+
+
+    /**
+     * 发布一条公告
+     * @param notice
+     */
+
+    @Insert("insert into notice values(null,#{noticeTime},#{title},#{content})")
+    @Options(useGeneratedKeys = true, keyProperty = "nid", keyColumn = "nid")
+    void addNotice(Notice notice);
 
 }
